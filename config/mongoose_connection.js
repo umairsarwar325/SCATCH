62b876a6c687950f622e4fe6104769b734c2ed4a
Umair Sarwar <umairsarwar4254@gmail.com>
2024-08-06 21:36:40 +0500

    added debugger for development phase testing

--- config/mongoose_connection.js
+++ config/mongoose_connection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongooose");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/scatch")
   .then(() => {
-    console.log("db connected");
+    dbgr("db connected");
   })
   .catch((error) => {
-    console.log(error);
+    dbgr(error);
   });
 
 module.exports = mongoose.connection;
